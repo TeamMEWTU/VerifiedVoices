3b1e2e798467d7c6ecd85375752748a9f5866b4d
Rabea Ahmad <rabeaahmad@macbook-air.mynetworksettings.com>
2022-12-19 21:19:29 -0500

    added linkedin OAuth functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
