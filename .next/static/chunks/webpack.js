57303cb5497e419d24fa6de216b2d49a00efb706
Erik Stynchula <estynchula@gmail.com>
2022-12-21 00:10:28 -0500

    completed communities functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "148886eaf242aa44"; }
+/******/ 		__webpack_require__.h = function() { return "f9844c4079ee9014"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
